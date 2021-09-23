4cafd3a557548f099b0727c0e148e076fcb71201
원치현 <heungeob1003@gmail.com>
2021-09-23 18:59:47 +0900

    Change Propery Writable

---
+++ ChangePropertyWritable.js
@@ -0,0 +1,9 @@
+var person = {
+    name:"Tom"
+};
+Object.defineProperty(person,"name",{
+    writable:false,
+});
+Object.getOwnPropertyDescriptor(person,"name");
+//->value:"Tom",writable:false,enumeralbe:true,configurable:true
+
