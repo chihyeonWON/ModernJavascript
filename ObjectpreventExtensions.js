b9829000ebc70552157f9fc686035c1eb9e958cb
원치현 <heungeob1003@gmail.com>
2021-09-25 19:30:17 +0900

    How to use ObjectpreventExtensions function to lock object

---
+++ ObjectpreventExtensions.js
@@ -0,0 +1,8 @@
+var person = {
+    name:"Tom"
+};
+Object.preventExtensions(person);
+person.age = 17;
+console.log("age" in person) // -> false
+
+console.log(Object.isExtensible(person)); // -> false
